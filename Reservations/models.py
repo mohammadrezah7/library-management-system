ce9cf01adbb64029d38560843a6fd8d7340fbfc7
mohammadreza <m.r.hassani8585@gmail.com>
2025-08-08 01:14:30 +0330

    improve database to save and show available books

--- Reservations/models.py
+++ Reservations/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.utils.functional import lazy
 from Books.models import book
 
-class reservations(models.Model):
+def get_book_choices():
     available_books = book.objects.filter(is_availabe=True)
     list_books = []
     for i in available_books:
         a = (i.title.upper(), f'{i.title}')
         list_books.append(a)
         a = ()
+    return list_books
+
+class reservations(models.Model):
     reserver_name = models.CharField(max_length=300, verbose_name='reserver_name', default="")
-    book_reserver = models.CharField(max_length=300, choices=list_books, default=list_books[0][0])
+    book_reserver = models.CharField(max_length=300, choices=lazy(get_book_choices, list)(),default="")
     reserver_time = models.DateTimeField(auto_now_add=True, verbose_name='reserver_time', null=True)
-# Create your models here.
