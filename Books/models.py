52eee4dd815172896a5091e3d2ac65478781a1d7
mohammadreza <m.r.hassani8585@gmail.com>
2025-08-07 21:00:28 +0330

    fix some bogs in homepage of website and modify forms.py for add or delete some books to database

--- Books/models.py
+++ Books/models.py
@@ -5,4 +5,7 @@ class book(models.Model):
     author = models.CharField(max_length=300, verbose_name='author')
     published_date = models.DateTimeField(auto_now_add=True, verbose_name='published_date')
     is_availabe = models.BooleanField(default=True, verbose_name='available/not available')
+
+    def __str__(self):
+        return f'{self.title}'
 # Create your models here.
--- Books/views.py
+++ Books/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import contactForm
+from django.contrib import messages
+from django.urls import reverse
 
 def book_page(response):
-    return render(response, 'Book/index2.html')
+    if response.method == 'POST':
+        form = contactForm(response.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(response, 'اطلاعات با موفقیت ثبت شد!')
+            return redirect(reverse('book_page'))
+    else:
+        form = contactForm()
+    
+    return render(response, 'Books/index2.html', {'form': form})
 
 # Create your views here.
